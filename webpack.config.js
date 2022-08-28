98b836f0343d18ad518157975b12e2d5b73b0eca
nimishgo <nimish.2711@gmail.com>
2022-08-28 21:41:27 +0530

    👍 initial commit

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+
+    mode: "development",
+    watch: true,
+    entry: {
+        main : path.resolve(__dirname,'./src/index.js'),
+    },
+
+    output: {
+        filename: '[name].js',
+        path : path.resolve(__dirname, 'dist'),
+    },
+
+    module : {
+        rules: [
+            {
+                test:/\.css$/,
+                use : [
+                    'style-loader',
+                    'css-loader',
+                ],
+            },
+            {
+                test: /\.(png|svg|gif|jpg|webp)$/i,
+                type: 'asset/resource',
+            }
+        ]
+    },
+};
