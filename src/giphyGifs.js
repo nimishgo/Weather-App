e366c229f1a878ded2d01078c5884bd2ab5493ac
nimishgo <nimish.2711@gmail.com>
2022-08-29 13:49:05 +0530

    fix: changed http to https

--- src/giphyGifs.js
+++ src/giphyGifs.js
@@ -3,7 +3,7 @@ const giphyKey = '1RrR64yeXUReOp4UeWAgYVWNUXlTVSLj';
 
 export async function genGifs(text) {
 
-        const res = await fetch(`http://api.giphy.com/v1/gifs/translate?api_key=${giphyKey}&s=${text}`,{mode:"cors"});
+        const res = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=${giphyKey}&s=${text}`,{mode:"cors"});
         // https://api.giphy.com/v1/gifs/translate?api_key=1RrR64yeXUReOp4UeWAgYVWNUXlTVSLj&s=${txt.value}`,
         
         const gifData = await res.json();
