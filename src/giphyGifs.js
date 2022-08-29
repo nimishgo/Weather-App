4286fdb61f565fb94d5654b23c6cdec129992047
nimishgo <nimish.2711@gmail.com>
2022-08-29 13:13:37 +0530

    feat: JS module to get gifs from giphy API

--- src/giphyGifs.js
+++ src/giphyGifs.js
@@ -3,10 +3,11 @@ const giphyKey = '1RrR64yeXUReOp4UeWAgYVWNUXlTVSLj';
 
 export async function genGifs(text) {
 
-        const res = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=${giphyKey}&s=${text}`,{mode:"cors"});
+        const res = await fetch(`http://api.giphy.com/v1/gifs/translate?api_key=${giphyKey}&s=${text}`,{mode:"cors"});
+        // https://api.giphy.com/v1/gifs/translate?api_key=1RrR64yeXUReOp4UeWAgYVWNUXlTVSLj&s=${txt.value}`,
         
         const gifData = await res.json();
-        
+        console.log(gifData);
         const url = gifData.data.images.original.url;
 
         return url;
