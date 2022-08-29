e0c21a3601dc6c01aff485c8cc06d1b881ab4106
nimishgo <nimish.2711@gmail.com>
2022-08-29 13:12:04 +0530

    add: url loader

--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,7 @@ module.exports = {
             },
             {
                 test: /\.(png|svg|gif|jpg|webp)$/i,
+                loader: 'url-loader',
                 type: 'asset/resource',
             }
         ]
